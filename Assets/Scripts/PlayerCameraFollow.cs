7199793ca45fec5591ba6997ab64763e8c2d69be
Jiklopo <43034313+Jiklopo@users.noreply.github.com>
2021-09-22 22:39:26 +0600

    added camera follow

---
+++ Assets/Scripts/PlayerCameraFollow.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class PlayerCameraFollow : MonoBehaviour
+{
+    private Transform target => PlayerController.parentPlayer.transform;
+    private Camera camera;
+
+    private void Awake()
+    {
+        camera = Camera.main;
+    }
+
+    private void LateUpdate()
+    {
+        var position = new Vector3(target.position.x, target.position.y, camera.transform.position.z);
+        camera.transform.position = position;
+    }
+}
