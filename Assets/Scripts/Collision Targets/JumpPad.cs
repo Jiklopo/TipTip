da7321dc18d8db713409c02456c1201ab68c2106
Jiklopo <43034313+Jiklopo@users.noreply.github.com>
2021-10-15 10:30:39 +0600

    changed jump pad

--- Assets/Scripts/Collision Targets/JumpPad.cs
+++ Assets/Scripts/Collision Targets/JumpPad.cs
@@ -1,11 +1,20 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 
 public class JumpPad : MonoBehaviour, ICollisionTarget
 {
+	private Animation animation;
 	[SerializeField] private float forceMultiplier = 10f;
+
+	private void Awake()
+	{
+		animation = GetComponent<Animation>();
+	}
+
 	public void OnCollision(GameObject other)
 	{
 		other.GetComponent<Rigidbody2D>()?.AddForce(Vector2.up * forceMultiplier, ForceMode2D.Impulse);
+		animation.Play();
 	}
 }
